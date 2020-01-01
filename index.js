9c87f3b4f3a5a98d47a212c3304030287ad6b420
ildanaruzybayeva <ildana.ruzybayeva@gmail.com>
2020-01-01 15:02:27 +0100

    timer logic done

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-console.log('Hello World!');
-console.log('Hello World!');
-console.log('Hello World!');
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
+  }
+
+  start = () => {
+    this.tick();
+    this.interval = setInterval(this.tick, 1000); //id=1
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  tick = () => {
+    this.timeRemaining <= 0
+      ? this.pause()
+      : (this.timeRemaining = this.timeRemaining - 1);
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
+}
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
